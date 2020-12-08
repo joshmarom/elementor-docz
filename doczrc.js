92b2247b1ab10c16a933ceb4e3136d0d17079e0d
Josh <Josh@elementor.com>
2020-12-08 04:21:35 +0200

    Test

--- doczrc.js
+++ doczrc.js
@@ -4,6 +4,6 @@ const { menuItems } = require(`${projectRoot}/config/menu.json`);
 
 export default {
   files: 'docs/**/*.{md,markdown,mdx}',
-  title: 'Docz & Netlify CMS',
+  title: 'Elementor docz',
   menu: menuItems
 }
